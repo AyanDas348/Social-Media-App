364bb08ab5637b9b87ce2f9030851a7674932f79
Ayan Das <dasayan348@gmail.com>
2023-03-05 22:58:42 +0530

    server running

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bodyParser from "bodyParser"
+import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
@@ -34,3 +34,13 @@ const storage = multer.diskStorage({
     }
 })
 const upload = multer({storage})
+
+
+//Mongoose setup
+const PORT = process.env.PORT || 6001
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() =>{
+    app.listen(PORT, () => console.log(`Server port: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`))
